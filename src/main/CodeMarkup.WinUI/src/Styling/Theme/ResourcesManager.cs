8cbf4c39dce91e1bb0368588197e637bf050ab51
Pawel Krzywdzinski <pawel@dexus.pl>
2023-03-24 19:05:39 +0100

    ResourcesManager

---
+++ src/main/CodeMarkup.WinUI/src/Styling/Theme/ResourcesManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
+
+namespace CodeMarkup.WinUI.Styling
+{
+    public class ResourcesManager
+    {
+        static readonly UISettings settings;
+        static ResourcesManager()
+        {
+            settings = new UISettings();
+            settings.ColorValuesChanged += settings_ColorValuesChanged;
+        }
+
+        private static void settings_ColorValuesChanged(UISettings sender, object args)
+        {
+            ThemeChanged?.Invoke();
+        }
+
+        internal static event Action ThemeChanged;
+
+        public static void UpdateTheme()
+        {
+            ThemeChanged();
+        }
+    }
+}
